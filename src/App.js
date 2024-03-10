b5f549c50c5a8cb8e140275c6cdc7074cc75eb95
calvin.abraham7@gmail.com <calvin.abraham7@gmail.com>
2024-03-10 23:44:51 +0530

    navigate while loading

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
+import Progress from './components/Progress';
 import Dashboard from './components/Dashboard';
 
-export default () => {
-  return <Dashboard />;
+export default ({ loading }) => {
+  return loading ? <Progress /> : <Dashboard />;
 };
